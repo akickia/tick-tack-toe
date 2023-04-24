9e920516734598175ff58d48b383c557069ab826
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-04-24 14:59:12 +0200

    Cleanup and level #1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
 import Footer from './Components/Footer'
 import Main from './Components/Main'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 
 function App() {
+  //Add fonts
   const theme = extendTheme({
     fonts: {
       heading: `'Bruno Ace SC', cursive`,
--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -3,26 +3,28 @@ import "./Board.scss"
 import { Box } from "@chakra-ui/react"
 
 export default function Board() {
-  const [boxPos, setBoxPos] = useState({x: 0, y: 0})
 
+  //Get coordinates of mouse when clicking and writing to console.
+  const [boxPos, setBoxPos] = useState({x: 0, y: 0})
   useEffect(() => {
     console.log(`Coordinates x: ${boxPos.x}, y: ${boxPos.y}`)
   }, [boxPos])
   const  handleOnclick = (event) => {
     setBoxPos({x:event.clientX, y:event.clientY})
-    
   }
+
+
   return (
     <section>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
-      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, pink.300)">box</Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
+      <Box onClick={(event) => {handleOnclick(event)}} cursor="pointer" display="flex" alignItems="center" justifyContent="center" w="100%" h="100%" bgGradient="linear(to-t, green.100, green.50, green.100)"></Box>
     </section>
   )
 }
